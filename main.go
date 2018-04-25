1c0da3506cecae2d0cf98b37392e565c66c16f9a
Navendra Jha <navendra@asu.edu>
2018-04-24 17:35:28 -0700

    Added Main Package instead of GoColumbus

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
-package GoColumbus
+package main
 
 import (
 	"encoding/json"
-	//"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"log"
 )
 
 var restaurants []Restaurant
 
-// our main function
-func main() {
-	router := mux.NewRouter()
-	//fmt.Println(reflect.TypeOf(router))
 
-	restaurants = append(restaurants, Restaurant{ID: "1", Name: "John", Address: &Address{City: "City X", State: "State X"}})
-	restaurants = append(restaurants, Restaurant{ID: "2", Name: "Koko",  Address: &Address{City: "City Z", State: "State Y"}})
-	restaurants = append(restaurants, Restaurant{ID: "3", Name: "Francis"})
-
-
-	router.HandleFunc("/restaurants", GetRestaurants).Methods("GET")
-	router.HandleFunc("/restaurants/{id}", GetSingleRestaurant).Methods("GET")
-	router.HandleFunc("/restaurants/{id}", CreateRestaurants).Methods("POST")
-	router.HandleFunc("/restaurants/{id}", DeleteRestaurants).Methods("DELETE")
-	http.ListenAndServe(":4646", router)
-
-	//log.Fatal(http.ListenAndServe(":4646", router)) // 4646 on PhoneDial stands for GoGo
-}
 
 func GetRestaurants(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(restaurants)
@@ -73,6 +56,25 @@ type Restaurant struct {
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
+}
+
+// our main function
+func main() {
+	router := mux.NewRouter()
+	//fmt.Println(reflect.TypeOf(router))
+
+	restaurants = append(restaurants, Restaurant{ID: "1", Name: "John", Address: &Address{City: "City X", State: "State X"}})
+	restaurants = append(restaurants, Restaurant{ID: "2", Name: "Koko",  Address: &Address{City: "City Z", State: "State Y"}})
+	restaurants = append(restaurants, Restaurant{ID: "3", Name: "Francis"})
+
+
+	router.HandleFunc("/restaurants", GetRestaurants).Methods("GET")
+	router.HandleFunc("/restaurants/{id}", GetSingleRestaurant).Methods("GET")
+	router.HandleFunc("/restaurants/{id}", CreateRestaurants).Methods("POST")
+	router.HandleFunc("/restaurants/{id}", DeleteRestaurants).Methods("DELETE")
+	//http.ListenAndServe(":4646", router)
+
+	log.Fatal(http.ListenAndServe(":4646", router)) // 4646 on PhoneDial stands for GoGo
 }
 
 
